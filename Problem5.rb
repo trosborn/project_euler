b8aaf4e7eac5f2289966d2d595c7b204bf36bd47
trosborn <trosborn@gmail.com>
2014-10-15 12:14:35 -0700

    working on probl 5

--- Problem5.rb
+++ Problem5.rb
@@ -1,16 +1,18 @@
 class Problem5
 
   def smallest_multiple
-    i = 2
     n = 1
-    ary = Array.new
-    while i <= 20
-      if i % n = 0 && i = 20
-        return ary
-      elsif i % n = 0
-        ary.push(n)
-        i += 1
-        n += 1
+    while n < 200000000 do
+      n += 1
+      i = 2
+      while i <= 20 do
+        if n % i == 0 && i == 20
+          return n
+        elsif n % i == 0
+          i += 1
+        else
+          break
+        end
       end
     end
   end
