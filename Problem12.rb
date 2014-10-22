4be527262f7658f387b7bca3888074c8424028cf
trosborn <trosborn@gmail.com>
2014-10-22 16:13:04 -0700

    starts problem 12

--- Problem12.rb
+++ Problem12.rb
@@ -1,7 +1,23 @@
 class Problem12
 
-  def triangular_number
-    xn = n(n+1)/2
+  def triangular_number num
+    n = num
+    x = n*(n+1)/2
+    factors(x)
+  end
+
+  def factors num
+    x = num
+    (1..x).select { |n| (x % n).zero? }
+  end
+
+  def check_divisors
+    x = 50
+    q = 10
+    until q == 50
+      x += 1
+      q = triangular_number(x)
+    end
   end
 
 end
