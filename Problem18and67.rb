b2919faf55cacab48cb467460ca9059743918c42
trosborn <trosborn@gmail.com>
2014-12-09 14:45:08 -0800

    renames probl 18 and finishes it and 67git status

---
+++ Problem18and67.rb
@@ -0,0 +1,27 @@
+def tree
+  ary = Array.new
+  f = File.open('Problem18.txt')
+  file = f.read
+  file.each_line do |line|
+    ary << line.split.map(&:to_i)
+  end
+  compare_sums(ary)
+end
+
+def compare_sums array
+  ary = array
+  i = ary.length - 1
+  while i > 0
+    n = ary[i].length - 1
+    while n > 0
+      if ary[i][n] > ary[i][n-1]
+        ary[i-1][n-1] = ary[i][n] + ary[i-1][n-1]
+      else
+        ary[i-1][n-1] = ary[i][n-1] + ary[i-1][n-1]
+      end
+      n -= 1
+    end
+    puts ary[0][0]
+    i -= 1
+  end
+end
