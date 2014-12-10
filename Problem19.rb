6913ed7338395ca248c3b7558103380346959e0f
Thomas Osborn <trosborn@gmail.com>
2014-12-09 22:44:18 -0800

    problem 19 off by one

--- Problem19.rb
+++ Problem19.rb
@@ -1,4 +1,34 @@
-def is_sunday?
-  self % 7 == 0
-  true
+def loop year
+  total_days = 366
+  sundays = 0
+  while year <= 2000
+    month = 1
+    while month <= 12
+      total_days += days_in_month(month, year)
+      unless year == 2000 && month == 12
+        if (total_days + 1) % 7 == 0
+          sundays += 1
+        end
+      end
+      month += 1
+    end
+    year += 1
+  end
+  sundays
+end
+
+def days_in_month month, year
+  if month == 2
+    num_days = 28 + leap_year(year)
+  else
+    num_days = 31 - (month - 1) % 7 % 2
+  end
+end
+
+def leap_year year
+  if year % 4 == 0 && year % 400 != 0
+    1
+  else
+    0
+  end
 end
